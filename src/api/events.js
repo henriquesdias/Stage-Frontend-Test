dbdf2c40a8703b68c701337a7510b0ec96a6be2a
henriquesdias <henrique.dias1915@gmail.com>
2023-09-07 19:22:38 -0300

    feat: user can read all events associated to a subprocess

--- src/api/events.js
+++ src/api/events.js
@@ -7,9 +7,13 @@ async function createEvent({ title, notes, date, time, subprocess_id }) {
     body: JSON.stringify({ title, notes, date, time }),
   });
 }
+async function getEvents(subprocess_id) {
+  return fetch(`${import.meta.env.VITE_BASE_URL}/events/${subprocess_id}`);
+}
 
 const eventsRequests = {
   createEvent,
+  getEvents,
 };
 
 export default eventsRequests;
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
+
 import { useNavigate } from "react-router-dom";
 import { GrAddCircle } from "react-icons/gr";
 
+import eventsRequests from "../api/events";
+
 export default function Events({ subprocess_id }) {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    eventsRequests
+      .getEvents(subprocess_id)
+      .catch((error) => console.log(error))
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setEvents([...data]);
+      });
+  }, [subprocess_id]);
+
+  console.log(events);
   const navigate = useNavigate();
   return (
     <div className="flex flex-col pl-8 w-full overflow-x-auto">
@@ -13,16 +30,16 @@ export default function Events({ subprocess_id }) {
         />
       </span>
       <div className="flex gap-8">
-        <div className="flex flex-col p-2 w-44 h-28 bg-[blue] rounded-xl gap-1 cursor-pointer">
-          <span>Título do evento</span>
-          <span>Data: 27/12/2025</span>
-          <span>Horário: 14h</span>
-        </div>
-        <div className="flex flex-col p-2 w-44 h-28 bg-[blue] rounded-xl gap-1 cursor-pointer">
-          <span>Título do evento</span>
-          <span>Data: 27/12/2025</span>
-          <span>Horário: 14h</span>
-        </div>
+        {events.map((e) => (
+          <div
+            className="flex flex-col justify-center items-center gap-3 p-2 w-44 h-28 bg-[#e3f5fe] rounded-xl cursor-pointer"
+            key={e.id}
+          >
+            <span>{e.title}</span>
+            <span>Date: {e.date.split("T")[0].replaceAll("-", "/")}</span>
+            <span>Time: {e.time}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
